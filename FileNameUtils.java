501efedf3440629334353639712749cdd8d417e8
ngocduc33 <43676655+ngocduc33@users.noreply.github.com>
2022-07-06 15:33:38 +0700

    Create FileNameUtils.java
    add utils

---
+++ FileNameUtils.java
@@ -0,0 +1,5 @@
+public class FileNameUtils {
+	public static final String DEFAULT = "slang.txt";
+	public static final String ORIGINAL = "original-slang.txt";
+    public static final String HISTORY = "history-slang.txt";
+}
